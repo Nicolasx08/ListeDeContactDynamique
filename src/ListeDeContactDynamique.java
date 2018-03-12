b95977d59c0bd18800426f5fa286342a29892c2b
Nicolas Pageau <nic.pageau@hotmail.com>
2018-03-12 15:34:34 -0400

    ListeDynamique #2

--- src/ListeDeContactDynamique.java
+++ src/ListeDeContactDynamique.java
@@ -5,35 +5,46 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Created by PagNi1731620 on 2018-02-26.
- */
-public class ListeDeContactDynamique {
-    public static void main(String[] args) {
 
-        int decision=0;
-        boolean quitter=false;
-        HashMap<String, Contact> map=new HashMap<>();
-        int nbeNum=0;
-        char autreNuméro='k';
-        boolean autreNum=true;
-        boolean rep=false;
-        Scanner sc= new Scanner(System.in);
-        while (quitter==false) {
-            System.out.println("1-Ajouter un contact.");
-            System.out.println("2-Modifier un contact.");
-            System.out.println("3-Afficher un contact.");
-            System.out.println("4-Quitter.");
-            decision = sc.nextInt();
-        }
-        switch (decision){
-            case 1:
-                Contact.creerContact();
-                Contact contact= Contact.creerContact();
-                map.put(contact.getNom(),contact);
+ public class ListeDeContactDynamique {
+ public static void main(String[] args) {
 
-
-                break;
-        }
+     boolean ok = false;
+     int decision = 0;
+     boolean quitter = false;
+     HashMap<String, Contact> map = new HashMap<>();
+     List<Telephone> listeTel = new ArrayList<>();
+     int nbeNum = 0;
+     String choix = "l";
+     char autreNuméro = 'k';
+     boolean autreNum = true;
+     boolean listePasFini = false;
+     Scanner sc = new Scanner(System.in);
+     while (!listePasFini) {
+         quitter=false;
+         while (!quitter) {
+             System.out.println("1-Ajouter un contact.");
+             System.out.println("2-Modifier un contact.");
+             System.out.println("3-Afficher un contact.");
+             System.out.println("4-Quitter.");
+             decision = sc.nextInt();
+             if (decision <= 7 && decision >= 1) {
+                 quitter = true;
+             } else {
+                 System.out.println("Entrez une réponse valide.");
+             }
+         }
+         switch (decision) {
+             case 1:
+                 Contact contact = Contact.creerContact();
+                 map.put(contact.getNom(), contact);
+                 break;
+             case 2:
+                 System.out.println("Quel contact voulez-vous modifier?");
+                 choix=sc.next();
+                 map.get(choix);
+                 System.out.println();
+         }
+     }
     }
-}
+ }
